a48ed8ddde908b57856ccab0398961009f673213
Insomniac10102 <ins10102@gmail.com>
2015-07-12 19:45:35 -0700

    Add google fullscreen google map.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 /*Copyright (C) Tyler Hackett 2015*/
 
+var map;
+
 /**
  *	Retrieve FB user's address.
  **/
 function GetLocation(user)
 {
-
+	
 }
 
 /**
  *	Convert postal address into longitude & latitude.
- *  A JSON object with "lng" and "lat" will be passed
+ *	A JSON object with "lng" and "lat" will be passed
  *	to callback on success, or false on fail.
  **/
 function GetGeocode(address, callback)
@@ -23,3 +25,13 @@ function GetGeocode(address, callback)
 			callback(false);
 	}, "json");
 }
+
+function InitializeMap() {
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    zoom: 8,
+    center: {lat: -34.397, lng: 150.644}
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', InitializeMap);
+
